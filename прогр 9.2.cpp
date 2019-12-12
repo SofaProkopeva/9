1cc4959641ed888c1852496e9ba6fe3eadfe7bc9
SofaProkopeva <55788579+SofaProkopeva@users.noreply.github.com>
2019-12-12 16:56:16 +0300

    Add files via upload

---
+++ прогр 9.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a;
+	int b, n;
+	n = 0;
+	cout << "Enter the initial direction and command." << endl;
+	cin >> a >> b;
+	if (a == 'n')
+		n = 0;
+	if (a == 'w')
+		n = 1;
+	if (a == 's')
+		n = 2;
+	if (a == 'e')
+		n = 3;
+	n = (n + b) % 4;
+	if (n == 0)
+		cout << "North";
+	if (n == 1)
+		cout << "West";
+	if (n == 2)
+		cout << "South";
+	if (n == 3)
+		cout << "East";
+}
